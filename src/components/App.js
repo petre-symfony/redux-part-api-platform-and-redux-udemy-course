e3de5b33a0967b8014cfe49fbea6271517268b53
petrero <petrero@gmail.com>
2020-07-24 08:54:19 +0300

    163. Header component (navigation)(Section 21: React Redux Application)

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Router, Route } from "react-router-dom";
 import history from '../history';
 import LoginForm from "./LoginForm";
 import BlogPostListContainer from "./BlogPostListContainer";
+import Header from "./Header";
 
 class App extends React.Component {
   render(){
     return (
       <div>
         <Router history={history}>
+          <Header />
           <Route path="/" exact component={BlogPostListContainer} />
           <Route path="/login" exact component={LoginForm}/>
         </Router>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class Header extends React.Component{
+  render(){
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">React Blog</Link>
+        <span className="navbar-text">
+          <Link to="/login">Sign In</Link>
+        </span>
+      </nav>
+    );
+  }
+}
+
+export default Header;
