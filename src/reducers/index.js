1045a5248910da879b43ff52e62dfc9cd1770735
petrero <petrero@gmail.com>
2020-07-24 19:36:40 +0300

    178. Storing JWT token in LocalStorage, Token Middleware(Section 21: React Redux Application)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import blogPostList from "./blogPostListReducer";
 import blogPost from "./blogPostReducer";
 import commentList from './commentListReducer';
+import auth from './auth';
 import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
   blogPostList,
   blogPost,
   commentList,
-  form: formReducer
+  form: formReducer,
+  auth
 })
