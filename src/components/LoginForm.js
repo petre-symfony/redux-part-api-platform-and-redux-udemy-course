4014a6e9136cd6bf117f2718ca9f230de9572654
petrero <petrero@gmail.com>
2020-07-24 18:42:24 +0300

    175. Render redux form component(Section 21: React Redux Application)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 
 class LoginForm extends React.Component {
+  renderInput({ input, label, type, meta: {error} }){
+    return (
+      <div className="form-group">
+        <label>{ label }</label>
+        <input type={type} { ...input } className="form-control"/>
+      </div>
+    )
+  }
+
   render(){
-    console.log(this.props);
-    return <div>Hello from LoginForm</div>;
+    return (
+      <div className="text-center">
+        <form className="mt-4">
+          <Field name="username" label="username" type="text" component={this.renderInput}/>
+          <Field name="password" label="password" type="password" component={this.renderInput}/>
+          <button type="submit" className="btn btn-primary btn-big btn-block">Log In</button>
+        </form>
+      </div>
+    );
   }
 }
 export default reduxForm({
