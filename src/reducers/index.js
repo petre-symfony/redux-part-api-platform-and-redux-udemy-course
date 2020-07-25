bdeb64eda5be7c35324251386ffe14ed7c4f4b9f
petrero <petrero@gmail.com>
2020-07-25 13:00:00 +0300

    181.1 Reading token in App component constructor(Section 21: React Redux Application)
    Read token in App component and redirect after a successfull login

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import commentList from './commentListReducer';
 import auth from './auth';
 import {reducer as formReducer} from 'redux-form';
 import { combineReducers } from "redux";
+import {routerReducer} from 'react-router-redux';
 
 export default combineReducers({
   blogPostList,
   blogPost,
   commentList,
   form: formReducer,
+  router: routerReducer,
   auth
 })
