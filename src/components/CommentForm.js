7995c5146d49a96ab91d04be41c8700aa3bc65b8
petrero <petrero@gmail.com>
2020-07-26 14:20:02 +0300

    188. Redux-form submitting state simulation(Section 21: React Redux Application)
    Simulate submitting the created comment.
    Both handleSubmit and submitting variables are provided by reduxForm.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, SubmissionError } from 'redux-form';
 import { connect } from 'react-redux';
 import { renderField } from "../form";
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 class CommentForm extends React.Component {
+  onSubmit = (values) => {
+    return sleep(5000).then(() => {
+      throw new SubmissionError({content: 'Invalid Coment'});
+    });
+  }
 
   render(){
+    const {handleSubmit, submitting} = this.props;
+
     return (
       <div className="card mb-3 mt-3 shadow-sm">
         <div className="card-body">
-          <form >
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
             <Field name="content" label="Type your comment" type="textarea" component={renderField}/>
-            <button type="submit" className="btn btn-primary btn-big btn-block">Add Comment</button>
+            <button type="submit" disabled={submitting} className="btn btn-primary btn-big btn-block">Add Comment</button>
           </form>
         </div>
       </div>
@@ -20,7 +29,7 @@ class CommentForm extends React.Component {
 }
 
 export default reduxForm({
-  'name': 'CommentForm'
+  'form': 'CommentForm'
 })(
   connect(
     null,
